154f10239389784db925cf61acb33dc851292063
inflaton <inflaton.sg@gmail.com>
2021-12-23 05:08:38 +0800

    2nd submission for KdTree

--- assignments/kdtrees/KdTree.java
+++ assignments/kdtrees/KdTree.java
@@ -6,13 +6,14 @@ import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
   private Node root; // root of BST
-  private final RectHV rootRect = new RectHV(0, 0, 1, 1);
+  private static final RectHV rootRect = new RectHV(0, 0, 1, 1);
 
-  private class Node {
+  private static class Node {
     private final Point2D point; // sorted by key
-    private Node left, right; // left and right subtrees
+    private Node left; // left and right subtrees
+    private Node right; // left and right subtrees
     private int size; // number of nodes in subtree
-    private boolean verticalSplit;
+    private final boolean verticalSplit;
 
     public Node(Point2D point, boolean verticalSplit, int size) {
       this.point = point;
@@ -36,8 +37,11 @@ public class KdTree {
 
   // return number of key-value pairs in BST rooted at x
   private int size(Node x) {
-    if (x == null) return 0;
-    else return x.size;
+    if (x == null) {
+      return 0;
+    } else {
+      return x.size;
+    }
   }
 
   // add the point to the set (if it is not already in the set)
@@ -49,10 +53,15 @@ public class KdTree {
   }
 
   private Node insert(Node node, Point2D p, boolean verticalSplit) {
-    if (node == null) return new Node(p, verticalSplit, 1);
+    if (node == null) {
+      return new Node(p, verticalSplit, 1);
+    }
     int cmp = compare(p, node.point, node.verticalSplit);
-    if (cmp < 0) node.left = insert(node.left, p, !verticalSplit);
-    else node.right = insert(node.right, p, !verticalSplit);
+    if (cmp < 0) {
+      node.left = insert(node.left, p, !verticalSplit);
+    } else {
+      node.right = insert(node.right, p, !verticalSplit);
+    }
     node.size = 1 + size(node.left) + size(node.right);
     return node;
   }
@@ -63,6 +72,7 @@ public class KdTree {
     }
     return Double.compare(p.y(), q.y());
   }
+
   // does the set contain point p?
   public boolean contains(Point2D p) {
     return contains(root, p);
@@ -72,10 +82,16 @@ public class KdTree {
     if (p == null) {
       throw new IllegalArgumentException();
     }
-    if (node == null) return false;
-    if (node.point.equals(p)) return true;
+    if (node == null) {
+      return false;
+    }
+    if (node.point.equals(p)) {
+      return true;
+    }
     int cmp = compare(p, node.point, node.verticalSplit);
-    if (cmp < 0) return contains(node.left, p);
+    if (cmp < 0) {
+      return contains(node.left, p);
+    }
     return contains(node.right, p);
   }
 
@@ -85,7 +101,9 @@ public class KdTree {
   }
 
   private void draw(Node node) {
-    if (node == null) return;
+    if (node == null) {
+      return;
+    }
     Point2D p = node.point;
     if (node.verticalSplit) {
       StdDraw.setPenColor(StdDraw.RED);
@@ -111,6 +129,24 @@ public class KdTree {
     return result;
   }
 
+  private void range(Node node, RectHV nodeRect, RectHV searchRect, Queue<Point2D> result) {
+    if (node == null) {
+      return;
+    }
+    Point2D p = node.point;
+    if (searchRect.contains(p)) {
+      result.enqueue(p);
+    }
+
+    Node[] childNodes = {node.left, node.right};
+    RectHV[] childRects = getChildRects(node, nodeRect);
+    for (int i = 0; i < childNodes.length; i++) {
+      if (searchRect.intersects(childRects[i])) {
+        range(childNodes[i], childRects[i], searchRect, result);
+      }
+    }
+  }
+
   private RectHV[] getChildRects(Node node, RectHV nodeRect) {
     Point2D p = node.point;
     RectHV leftRect =
@@ -124,26 +160,15 @@ public class KdTree {
     return new RectHV[] {leftRect, rightRect};
   }
 
-  private void range(Node node, RectHV nodeRect, RectHV searchRect, Queue<Point2D> result) {
-    if (node == null) return;
-    Point2D p = node.point;
-    if (searchRect.contains(p)) result.enqueue(p);
-
-    Node[] childNodes = {node.left, node.right};
-    RectHV[] childRects = getChildRects(node, nodeRect);
-    for (int i = 0; i < childNodes.length; i++) {
-      if (searchRect.intersects(childRects[i]))
-        range(childNodes[i], childRects[i], searchRect, result);
-    }
-  }
-
   // a nearest neighbor in the set to point p; null if the set is empty
   public Point2D nearest(Point2D p) {
     return nearest(root, rootRect, p, new Point2D(-1, -1));
   }
 
   private Point2D nearest(Node node, RectHV nodeRect, Point2D searchPoint, Point2D closetPoint) {
-    if (node == null) return closetPoint;
+    if (node == null) {
+      return closetPoint;
+    }
     Point2D p = node.point;
     double shortestDistanceSquared = searchPoint.distanceSquaredTo(closetPoint);
     double distanceSquared = searchPoint.distanceSquaredTo(p);
@@ -152,32 +177,36 @@ public class KdTree {
       shortestDistanceSquared = distanceSquared;
     }
 
-    double[] childDistancesSquared = new double[2];
-    Node[] childNodes = {node.left, node.right};
-    RectHV[] childRects = getChildRects(node, nodeRect);
-    double min = Double.POSITIVE_INFINITY;
-    int index = 0;
-    for (int i = 0; i < childNodes.length; i++) {
-      childDistancesSquared[i] = distanceSquaredToRect(searchPoint, childRects[i]);
-      if (min > childDistancesSquared[i]) {
-        index = i;
-        min = childDistancesSquared[i];
+    if (shortestDistanceSquared < distanceSquaredToRect(searchPoint, nodeRect)) {
+      double[] childDistancesSquared = new double[2];
+      Node[] childNodes = {node.left, node.right};
+      RectHV[] childRects = getChildRects(node, nodeRect);
+      double min = Double.POSITIVE_INFINITY;
+      int index = 0;
+      for (int i = 0; i < childNodes.length; i++) {
+        childDistancesSquared[i] = distanceSquaredToRect(searchPoint, childRects[i]);
+        if (min > childDistancesSquared[i]) {
+          index = i;
+          min = childDistancesSquared[i];
+        }
       }
-    }
-    for (int i = 0; i < 2; i++) {
-      if (childDistancesSquared[i] < shortestDistanceSquared) {
-        closetPoint = nearest(childNodes[index], childRects[index], searchPoint, closetPoint);
-        shortestDistanceSquared = searchPoint.distanceSquaredTo(closetPoint);
+      for (int i = 0; i < 2; i++) {
+        if (childDistancesSquared[i] < shortestDistanceSquared) {
+          closetPoint = nearest(childNodes[index], childRects[index], searchPoint, closetPoint);
+          shortestDistanceSquared = searchPoint.distanceSquaredTo(closetPoint);
+        }
+        index = 1 - index;
       }
-      index = 1 - index;
     }
     return closetPoint;
   }
 
   private double distanceSquaredToRect(Point2D point, RectHV rect) {
-    if (rect.contains(point)) return 0;
+    if (rect.contains(point)) {
+      return 0;
+    }
 
-    double dxSquared, dySquared;
+    double dxSquared;
     if (point.x() < rect.xmin()) {
       dxSquared = (point.x() - rect.xmin()) * (point.x() - rect.xmin());
     } else {
@@ -187,6 +216,7 @@ public class KdTree {
       return dxSquared;
     }
 
+    double dySquared;
     if (point.y() < rect.ymin()) {
       dySquared = (point.y() - rect.ymin()) * (point.y() - rect.ymin());
     } else {
@@ -219,8 +249,10 @@ public class KdTree {
   }
 
   private static void processRangeSearchQueries(KdTree kdtree) {
-    double x0 = 0.0, y0 = 0.0; // initial endpoint of rectangle
-    double x1 = 0.0, y1 = 0.0; // current location of mouse
+    double x0 = 0.0;
+    double y0 = 0.0; // initial endpoint of rectangle
+    double x1 = 0.0;
+    double y1 = 0.0; // current location of mouse
     boolean isDragging = false; // is the user dragging a rectangle
 
     // draw the points
@@ -234,21 +266,18 @@ public class KdTree {
     StdDraw.enableDoubleBuffering();
 
     while (true) {
+
       // user starts to drag a rectangle
       if (StdDraw.isMousePressed() && !isDragging) {
-        x0 = x1 = StdDraw.mouseX();
-        y0 = y1 = StdDraw.mouseY();
-        isDragging = true;
-      }
-
-      // user is dragging a rectangle
-      else if (StdDraw.isMousePressed() && isDragging) {
         x1 = StdDraw.mouseX();
         y1 = StdDraw.mouseY();
-      }
-
-      // user stops dragging rectangle
-      else if (!StdDraw.isMousePressed() && isDragging) {
+        x0 = x1;
+        y0 = y1;
+        isDragging = true;
+      } else if (StdDraw.isMousePressed()) {
+        x1 = StdDraw.mouseX();
+        y1 = StdDraw.mouseY();
+      } else if (isDragging) {
         isDragging = false;
       }
 
@@ -268,7 +297,9 @@ public class KdTree {
       // draw the range search results for kdtree-force data structure in red
       StdDraw.setPenRadius(0.03);
       StdDraw.setPenColor(StdDraw.RED);
-      for (Point2D p : kdtree.range(rect)) p.draw();
+      for (Point2D p : kdtree.range(rect)) {
+        p.draw();
+      }
 
       StdDraw.show();
       StdDraw.pause(20);
@@ -280,20 +311,19 @@ public class KdTree {
     StdDraw.enableDoubleBuffering();
     while (true) {
 
-      // the location (x, y) of the mouse
-      double x = StdDraw.mouseX();
-      double y = StdDraw.mouseY();
-      Point2D query = new Point2D(x, y);
-
       // draw all of the points
       StdDraw.clear();
       StdDraw.setPenColor(StdDraw.BLACK);
       StdDraw.setPenRadius(0.01);
       kdtree.draw();
 
-      // draw in red the nearest neighbor (using kdtree-force algorithm)
       StdDraw.setPenRadius(0.03);
       StdDraw.setPenColor(StdDraw.RED);
+
+      // the location (x, y) of the mouse
+      double x = StdDraw.mouseX();
+      double y = StdDraw.mouseY();
+      Point2D query = new Point2D(x, y);
       kdtree.nearest(query).draw();
 
       StdDraw.show();
--- assignments/kdtrees/PointSET.java
+++ assignments/kdtrees/PointSET.java
@@ -6,21 +6,21 @@ import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class PointSET {
-  private SET<Point2D> point2DSET;
+  private final SET<Point2D> points;
 
   // construct an empty set of points
   public PointSET() {
-    point2DSET = new SET<>();
+    points = new SET<>();
   }
 
   // is the set empty?
   public boolean isEmpty() {
-    return point2DSET.isEmpty();
+    return points.isEmpty();
   }
 
   // number of points in the set
   public int size() {
-    return point2DSET.size();
+    return points.size();
   }
 
   // add the point to the set (if it is not already in the set)
@@ -28,7 +28,7 @@ public class PointSET {
     if (p == null) {
       throw new IllegalArgumentException();
     }
-    point2DSET.add(p);
+    points.add(p);
   }
 
   // does the set contain point p?
@@ -36,12 +36,12 @@ public class PointSET {
     if (p == null) {
       throw new IllegalArgumentException();
     }
-    return point2DSET.contains(p);
+    return points.contains(p);
   }
 
   // draw all points to standard draw
   public void draw() {
-    for (Point2D p : point2DSET) {
+    for (Point2D p : points) {
       p.draw();
     }
   }
@@ -52,7 +52,7 @@ public class PointSET {
       throw new IllegalArgumentException();
     }
     Queue<Point2D> result = new Queue<>();
-    for (Point2D point2D : point2DSET) {
+    for (Point2D point2D : points) {
       if (rect.contains(point2D)) {
         result.enqueue(point2D);
       }
@@ -64,7 +64,7 @@ public class PointSET {
   public Point2D nearest(Point2D p) {
     Point2D nearest = null;
     double minDistanceSquared = Double.POSITIVE_INFINITY;
-    for (Point2D point2D : point2DSET) {
+    for (Point2D point2D : points) {
       double distanceSquared = p.distanceSquaredTo(point2D);
       if (minDistanceSquared > distanceSquared) {
         minDistanceSquared = distanceSquared;
@@ -94,8 +94,10 @@ public class PointSET {
   }
 
   private static void processRangeSearchQueries(PointSET brute) {
-    double x0 = 0.0, y0 = 0.0; // initial endpoint of rectangle
-    double x1 = 0.0, y1 = 0.0; // current location of mouse
+    double x0 = 0.0;
+    double y0 = 0.0; // initial endpoint of rectangle
+    double x1 = 0.0;
+    double y1 = 0.0; // current location of mouse
     boolean isDragging = false; // is the user dragging a rectangle
 
     // draw the points
@@ -112,19 +114,15 @@ public class PointSET {
 
       // user starts to drag a rectangle
       if (StdDraw.isMousePressed() && !isDragging) {
-        x0 = x1 = StdDraw.mouseX();
-        y0 = y1 = StdDraw.mouseY();
-        isDragging = true;
-      }
-
-      // user is dragging a rectangle
-      else if (StdDraw.isMousePressed() && isDragging) {
         x1 = StdDraw.mouseX();
         y1 = StdDraw.mouseY();
-      }
-
-      // user stops dragging rectangle
-      else if (!StdDraw.isMousePressed() && isDragging) {
+        x0 = x1;
+        y0 = y1;
+        isDragging = true;
+      } else if (StdDraw.isMousePressed()) {
+        x1 = StdDraw.mouseX();
+        y1 = StdDraw.mouseY();
+      } else if (isDragging) {
         isDragging = false;
       }
 
@@ -144,7 +142,9 @@ public class PointSET {
       // draw the range search results for brute-force data structure in red
       StdDraw.setPenRadius(0.03);
       StdDraw.setPenColor(StdDraw.RED);
-      for (Point2D p : brute.range(rect)) p.draw();
+      for (Point2D p : brute.range(rect)) {
+        p.draw();
+      }
 
       StdDraw.show();
       StdDraw.pause(20);
@@ -156,11 +156,6 @@ public class PointSET {
     StdDraw.enableDoubleBuffering();
     while (true) {
 
-      // the location (x, y) of the mouse
-      double x = StdDraw.mouseX();
-      double y = StdDraw.mouseY();
-      Point2D query = new Point2D(x, y);
-
       // draw all of the points
       StdDraw.clear();
       StdDraw.setPenColor(StdDraw.BLACK);
@@ -170,6 +165,11 @@ public class PointSET {
       // draw in red the nearest neighbor (using brute-force algorithm)
       StdDraw.setPenRadius(0.03);
       StdDraw.setPenColor(StdDraw.RED);
+
+      // the location (x, y) of the mouse
+      double x = StdDraw.mouseX();
+      double y = StdDraw.mouseY();
+      Point2D query = new Point2D(x, y);
       brute.nearest(query).draw();
 
       StdDraw.show();
