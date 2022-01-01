c2a61d8022bae791dc63d82e03971bd847f9ff92
Donghao Huang <donghao.huang@mastercard.com>
2022-01-01 19:49:59 +0800

    added classes for strong connected components (SCC)

--- tools/debug.sh
+++ tools/debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`/..
-java -ea -Xms512m -Xmx32g -classpath build/production/algs4:build/classes/java/main "$@"
+java -ea -Xms512m -Xmx32g -classpath build/production/assignments:build/classes/java/main "$@"
--- tools/run.sh
+++ tools/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`/..
-java -Xms512m -Xmx8g -classpath build/production/algs4:build/classes/java/main "$@"
+java -Xms512m -Xmx8g -classpath build/production/assignments:build/classes/java/main "$@"
