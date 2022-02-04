4534e5550cc0a40fdb883abf78acb979d221624a
Donghao Huang <donghao.huang@mastercard.com>
2022-02-05 07:58:36 +0800

    Burrows–Wheeler - timing inverseTransform

--- assignments/burrows/BurrowsWheeler.java
+++ assignments/burrows/BurrowsWheeler.java
@@ -25,56 +25,9 @@ public class BurrowsWheeler {
   // apply Burrows-Wheeler inverse transform,
   // reading from standard input and writing to standard output
   public static void inverseTransform() {
-    bruteForce();
-  }
-
-  public static void inverseTransformTimed(boolean bruteForce) {
-    Stopwatch sw = new Stopwatch();
-
-    if (bruteForce) {
-      bruteForce();
-    } else {
-      inverseTransform();
-    }
-
-    StdOut.println("\nbruteForce: " + bruteForce + "\nelapsedTime: " + sw.elapsedTime());
-    BinaryStdOut.close();
-  }
-
-  private static void bruteForce() {
     final int first = BinaryStdIn.readInt();
-    String input = BinaryStdIn.readString();
-    char[] t = input.toCharArray();
 
-    Integer[] s = new Integer[t.length];
-    int i;
-    for (i = 0; i < t.length; i++) {
-      s[i] = Integer.valueOf(t[i]);
-    }
-    MergeX.sort(s);
-
-    int[] next = new int[t.length];
-    boolean[] marked = new boolean[t.length];
-    for (i = 0; i < t.length; i++) {
-      for (int j = 0; j < t.length; j++) {
-        if (!marked[j]) {
-          if (s[i] == t[j]) {
-            marked[j] = true;
-            next[i] = j;
-            break;
-          }
-        }
-      }
-    }
-
-    char[] output = new char[input.length()];
-    int pointer = first;
-    for (i = 0; i < t.length; i++) {
-      output[i] = (char) s[pointer].intValue();
-      pointer = next[pointer];
-    }
-
-    BinaryStdOut.write(String.valueOf(output));
+    //    BinaryStdOut.write(String.valueOf(output));
     BinaryStdOut.flush();
   }
 
@@ -85,10 +38,6 @@ public class BurrowsWheeler {
       transform();
     } else if (args[0].equals("+")) {
       inverseTransform();
-    } else if (args[0].equals("a")) {
-      inverseTransformTimed(false);
-    } else if (args[0].equals("b")) {
-      inverseTransformTimed(true);
     }
   }
 }
